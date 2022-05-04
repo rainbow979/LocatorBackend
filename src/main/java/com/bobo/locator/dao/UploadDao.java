063c336c2eb6a7d4ffd9b3959cc5c606e18404d9
flowingwings <38078537+flowingwings@users.noreply.github.com>
2022-05-04 19:20:15 +0800

    Changed column name: device_id -> router_id.

--- src/main/java/com/bobo/locator/dao/UploadDao.java
+++ src/main/java/com/bobo/locator/dao/UploadDao.java
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UploadDao {
     @Insert("insert into message " +
-            "(device_id, time, mac, rssi, distance) values " +
+            "(router_id, time, mac, rssi, distance) values " +
             "(#{id, jdbcType=VARCHAR}, " +
             "#{time, jdbcType=VARCHAR}, #{deviceData.mac, jdbcType=VARCHAR}, " +
             "#{deviceData.rssi, jdbcType=VARCHAR}, #{deviceData.range, jdbcType=VARCHAR})")
     void addMessage(String id, String time, DeviceData deviceData);
 
     @Insert("insert into message " +
-            "(device_id, time, mac, rssi, distance) values " +
+            "(router_id, time, mac, rssi, distance) values " +
             "('0029c591', 'Tue Feb 21 08:13:31 2017', 'a4:56:02:61:7f:57', '-91', '91.5')")
     void testAdd();
 }
